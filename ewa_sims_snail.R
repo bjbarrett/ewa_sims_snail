ce646be58043b024d31840ec2370147491805116
Brendan Barrett <bbarrett@ab.mpg.de>
2021-02-19 13:38:00 +0100

    added function for average dynamics simulations

--- ewa_sims_snail.R
+++ ewa_sims_snail.R
@@ -15,13 +15,17 @@ col.pal <- c("#1B9E77", "#D95F02")
 dsim <- data.frame( timestep=0 , tech=0 , payoff_i1=0 , payoff_i2=0, A1=0 , A2=0 , Pr1=0 , Pr2=0)
 timesteps <- 100
 phi <- 0.1
-lambda <- 0.3
-techmeans <- c(8,10)
+lambda <- 0.2
+techmeans <- c(8,12)
 techvar <- c(1,1)
+#visualize how hard to learn
+plot(density(rnorm( 10000, mean=techmeans[1] , sd=techvar[1] ) ) ,col=col.pal[1] , xlim=c(0,20)) 
+lines(density(rnorm( 10000, mean=techmeans[2] , sd=techvar[2] ) ) ,col=col.pal[2]) 
 
 AC <- matrix(0,nrow=timesteps,ncol=2) 	
 
 ##simulated data for one discreet individual
+
 for (t in 1:timesteps){
   prtech_i <-  Softmax(lambda*AC[t,]) #calculate probability of performing a behavior at this timestep
   tech <- sample( 1:2 , size=1 , prob=prtech_i) # sample a behavior with prtech_i
@@ -39,9 +43,63 @@ for (t in 1:timesteps){
   }
 }
 
-#below plots sims
+#below plots sim
 plot(dsim$Pr1~dsim$timestep , col=col.pal[1] , pch=19 , xlab="timestep" , ylab="prob choose behavior", ylim=c(0,1.1) ) 
 points(dsim$Pr2~dsim$timestep,col=col.pal[2],pch=19 )
 points(rep(1.05,timesteps) ~ dsim$timestep,col=col.pal[dsim$tech],pch=19 )
 abline(h=1)
 
+####### mean dynamics of model i, no variance, just a numerical solution
+dsim2 <- data.frame( timestep=0 , A1=0 , A2=0 , Pr1=0 , Pr2=0)
+
+for (t in 1:timesteps){
+  prtech_i <-  Softmax(lambda*AC[t,]) #calculate probability of performing a behavior at this timestep
+  obs_payoffs_t <- techmeans #initialize observed payoffs vector
+
+  dsim2[t,] <- c(  t , AC[t,1] , AC[t,2] ,  prtech_i[1] , prtech_i[2] )
+  
+  # update attractions for next timestep t + 1, don't do on final round
+  if(t<timesteps){ 
+    for (k in 1:2){
+      AC[t+1,k] <- (1-phi)*AC[t,k] + phi*obs_payoffs_t[k]
+    }
+  }
+}
+
+#below plots sims
+plot(dsim2$Pr1~dsim2$timestep , col=col.pal[1] , pch=19 , xlab="timestep" , ylab="prob choose behavior", ylim=c(0,1) ) 
+points(dsim2$Pr2~dsim2$timestep,col=col.pal[2],pch=19 )
+
+
+###lets write a simple function to plot dynamics on average
+
+sim_reinf_learn <- function(phi,lambda){
+  techmeans <- c(8,12)
+  timesteps <- 100
+  dsim2 <- data.frame( timestep=0 , A1=0 , A2=0 , Pr1=0 , Pr2=0)
+  
+  for (t in 1:timesteps){
+    prtech_i <-  Softmax(lambda*AC[t,]) #calculate probability of performing a behavior at this timestep
+    obs_payoffs_t <- techmeans #initialize observed payoffs vector
+    dsim2[t,] <- c(  t , AC[t,1] , AC[t,2] ,  prtech_i[1] , prtech_i[2] )
+    # update attractions for next timestep t + 1, don't do on final round
+    if(t<timesteps){ 
+      for (k in 1:2){
+        AC[t+1,k] <- (1-phi)*AC[t,k] + phi*obs_payoffs_t[k]
+      }
+    }
+  }
+  #below plots sims
+  plot(dsim2$Pr1~dsim2$timestep , col=col.pal[1] , pch=19 , xlab="timestep" , ylab="prob choose behavior", ylim=c(0,1) ) 
+  points(dsim2$Pr2~dsim2$timestep,col=col.pal[2],pch=19 )
+  
+}
+
+##run some functions to exploire average dynamics across simulation space
+sim_reinf_learn(phi=0.5,lambda=0.1)
+sim_reinf_learn(phi=0.9,lambda=0.1)
+sim_reinf_learn(phi=0.01,lambda=0.3)
+sim_reinf_learn(phi=0.1,lambda=0.3)
+sim_reinf_learn(phi=0.5,lambda=0.3)
+sim_reinf_learn(phi=0.1,lambda=1)
+sim_reinf_learn(phi=0.9,lambda=1)
