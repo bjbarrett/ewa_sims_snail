a257be65fa810cb68502e9423c421279916937e1
Zoë <zgoldsborough@outlook.com>
2021-02-25 16:40:00 +0100

    tried looping over individuals

--- ewa_sims_snail.R
+++ ewa_sims_snail.R
@@ -28,21 +28,21 @@ AC <- matrix(0,nrow=timesteps,ncol=2)
 ##simulated data for one discreet individual
 
 for (t in 1:timesteps){
-  prtech_i <-  Softmax(lambda*AC[t,]) #calculate probability of performing a behavior at this timestep
-  tech <- sample( 1:2 , size=1 , prob=prtech_i) # sample a behavior with prtech_i
-  payoff <- rnorm( 1 , mean=techmeans[tech] , sd=techvar[tech] ) #draw a behavior of tech=k with specified mean and SD, realized choice
-  obs_payoffs_t <- rep(0,2) #initialize observed payoffs vector
-  obs_payoffs_t[tech] <- payoff #populate with each observed payoff
-  
-  dsim[t,] <- c(  t , tech , obs_payoffs_t[1] , obs_payoffs_t[2] , AC[t,1] , AC[t,2] ,  prtech_i[1] , prtech_i[2] )
-  
-  # update attractions for next timestep t + 1, don't do on final round
-   if(t<timesteps){ 
-    for (k in 1:2){
-      AC[t+1,k] <- (1-phi)*AC[t,k] + phi*obs_payoffs_t[k]
+    prtech_i <-  Softmax(lambda*AC[t,]) #calculate probability of performing a behavior at this timestep
+    tech <- sample( 1:2 , size=1 , prob=prtech_i) # sample a behavior with prtech_i
+    payoff <- rnorm( 1 , mean=techmeans[tech] , sd=techvar[tech] ) #draw a behavior of tech=k with specified mean and SD, realized choice
+    obs_payoffs_t <- rep(0,2) #initialize observed payoffs vector
+    obs_payoffs_t[tech] <- payoff #populate with each observed payoff
+    
+    dsim[t,] <- c(  t , tech , obs_payoffs_t[1] , obs_payoffs_t[2] , AC[t,1] , AC[t,2] ,  prtech_i[1] , prtech_i[2] )
+    
+    # update attractions for next timestep t + 1, don't do on final round
+     if(t<timesteps){ 
+      for (k in 1:2){
+        AC[t+1,k] <- (1-phi)*AC[t,k] + phi*obs_payoffs_t[k]
+      }
     }
   }
-}
 
 #below plots sim
 plot(dsim$Pr1~dsim$timestep , col=col.pal[1] , pch=19 , xlab="timestep" , ylab="prob choose behavior", ylim=c(0,1.1) ) 
@@ -105,3 +105,50 @@ sim_reinf_learn(phi=0.5,lambda=0.3)
 sim_reinf_learn(phi=0.1,lambda=1)
 sim_reinf_learn(phi=0.9,lambda=1)
 
+
+####### Loop over individuals ######
+
+individuals <- 5 # number of individuals 
+timesteps <- 100 # number of timesteps (t), in snail example, every interaction with a snail is a new timestep
+phi <- 0.1 # low is more reliance on past memory and high is reliance on recent experience (0-1)
+lambda <- 0.2 # sensitivity to differences in attraction, low is insensitive (0 is no attention to differences in attraction score, equal chance to pick each behavior), higher is more sensitive (0-infinity)
+techmeans <- c(8,12) # pay offs of behaviors 1 and 2 
+#techmeans_bas <- c(8,12) # pay offs of behaviors 1 and 2 baseline
+#techmeans_exp <- c(12,8) # pay offs switched (experiment)
+techvar <- c(1,1) # variance in pay offs of behavior across time steps
+AR <- array(0 , dim=c( nrow=timesteps , 2 , individuals ) )
+
+##simulated data looping over individuals
+dsim <- data.frame(individual = 0, timestep=0 , tech=0 , payoff_i1=0 , payoff_i2=0, A1=0 , A2=0 , Pr1=0 , Pr2=0)
+therow <- 1
+phi2 <- rnorm(individuals, mean = 0.5, sd = 0.5) # check in other code Brendan how he did it
+
+for (t in 1:timesteps){
+  for (i in 1:individuals) {
+    prtech_i <-  Softmax(lambda*AR[t,,i]) #calculate probability of performing a behavior at this timestep
+    tech <- sample( 1:2 , size=1 , prob=prtech_i) # sample a behavior with prtech_i
+    
+    #techmeans <- if(t > timesteps/2, techmeans_exp, techmeans_bas) # google this to figure out how to say that if timesteps more than half, use techmeans_exp .. 
+    
+    payoff <- rnorm( 1 , mean=techmeans[tech] , sd=techvar[tech] ) #draw a behavior of tech=k with specified mean and SD, realized choice
+    obs_payoffs_t <- rep(0,2) #initialize observed payoffs vector
+    obs_payoffs_t[tech] <- payoff #populate with each observed payoff
+    
+    dsim[therow,] <- c(i,  t , tech , obs_payoffs_t[1] , obs_payoffs_t[2] , AR[t,1,i] , AR[t,2,i] ,  prtech_i[1] , prtech_i[2] )
+    therow <- therow + 1
+    # update attractions for next timestep t + 1, don't do on final round
+    if(t<timesteps){ 
+      for (k in 1:2){
+        AR[t+1,k,i] <- (1-phi2[i])*AR[t,k,i] + phi2[i]*obs_payoffs_t[k]
+      }
+    }
+  }
+}
+
+#below plots sim, can loop over individuals
+for (i in 1:individuals) {
+  plot(dsim$Pr1[dsim$individual == i] ~dsim$timestep[dsim$individual == i] , col=col.pal[1] , pch=19 , xlab="timestep" , ylab="prob choose behavior", ylim=c(0,1.1) ) 
+  points(dsim$Pr2[dsim$individual == i]~dsim$timestep[dsim$individual == i],col=col.pal[2],pch=19 )
+  points(rep(1.05,timesteps) ~ dsim$timestep[dsim$individual == i],col=col.pal[dsim$tech[dsim$individual == i]],pch=19 )
+  abline(h=1)
+} 
